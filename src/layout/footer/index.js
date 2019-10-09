0be7311a7da53803be41a4ebaaab19bfbd9cb5a0
kdev <kdev.software1110@gmail.com>
2019-10-09 05:38:16 -0400

    header and footer

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -4,8 +4,9 @@ class AppFooter extends Component {
   render() {
     return (
       <div className="bg-dark text-white fixed-bottom py-3 text-center">
-        Copyright &copy; {new Date().getFullYear()} All rights reserved | built
-        by kdevsoftware
+        {/* Copyright &copy; {new Date().getFullYear()} All rights reserved | built
+        by kdevsoftware */}
+        {new Date().getFullYear()} - kdevsoftware
       </div>
     );
   }
--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Clock from 'react-live-clock';
 
 import Logo from '../../asset/image/logo.png';
 
@@ -6,9 +7,17 @@ class AppHeader extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-primary">
-        <a className="navbar-brand" href="/">
-          <img src={Logo} width="50" height="50" alt="" />
+        <a className="navbar-brand d-flex align-items-center py-3" href="/">
+          <img src={Logo} height="48" alt="" />
+          <h3 className="d-inline mb-0 ml-3">Todo List</h3>
         </a>
+
+        <Clock
+          className="text-white"
+          format={'h:mm:ss A ( MMM Mo, ddd )'}
+          ticking={true}
+          interval={1000}
+        />
       </nav>
     );
   }
