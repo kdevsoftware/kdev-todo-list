48bdee0515f8cf350862aab711331a8a73b2cbb2
kdev <kdev.software1110@gmail.com>
2019-10-09 02:41:23 -0400

    todo list - input todo content

--- src/layout/todo-list/index.js
+++ src/layout/todo-list/index.js
@@ -1,9 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class TodoList extends Component {
+  state = {
+    showForm: false,
+    formValue: ''
+  };
+
+  inputChange = event => {
+    this.setState({ formValue: event.target.value });
+  };
+
   render() {
-    return <div>Todo list works!</div>;
+    const { showForm } = this.state;
+
+    return (
+      <div>
+        <div>{this.renderForm()}</div>
+
+        <div>
+          <button onClick={() => this.setState({ showForm: !showForm })}>
+            {showForm ? <i>-</i> : <i>+</i>}
+          </button>
+        </div>
+      </div>
+    );
   }
+
+  renderForm = () => {
+    const { showForm, formValue } = this.state;
+
+    if (showForm) {
+      return (
+        <div>
+          <form onSubmit={this.formSubmit}>
+            <div>
+              <input
+                value={formValue}
+                onChange={this.inputChange}
+                id="toDoNext"
+                type="text"
+              />
+              <label htmlFor="toDoNext">What Next?</label>
+            </div>
+          </form>
+        </div>
+      );
+    }
+  };
 }
 
 export default TodoList;
