eddf7f089ac03f57ffdb6c496175fe15cd6435b7
kdev <kdev.software1110@gmail.com>
2019-10-09 07:53:01 -0400

    update logo href

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -7,7 +7,8 @@ class AppHeader extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-primary pl-4 pr-5">
-        <a className="navbar-brand d-flex align-items-center py-3" href="/">
+        {/* <a className="navbar-brand d-flex align-items-center py-3" href="/"> */}
+        <a className="navbar-brand d-flex align-items-center py-3" href="/kdev-todo-list">
           <img src={Logo} height="48" alt="" />
           <h3 className="d-inline mb-0 ml-3">Todo List</h3>
         </a>
