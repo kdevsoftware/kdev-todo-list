1a4504f37f20e0cee7a4e827ebe2773f85fdd316
kdev <kdev.software1110@gmail.com>
2019-10-09 05:11:35 -0400

    update main font

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -5,7 +5,7 @@ import Logo from '../../asset/image/logo.png';
 class AppHeader extends Component {
   render() {
     return (
-      <nav class="navbar navbar-dark bg-dark">
+      <nav class="navbar navbar-dark bg-primary">
         <a class="navbar-brand" href="/">
           <img src={Logo} width="50" height="50" alt="" />
         </a>
