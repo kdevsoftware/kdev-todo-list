7550d4946e53549f6ef0f3d2d6f9b9c58b762a4b
kdev <kdev.software1110@gmail.com>
2019-10-09 05:07:33 -0400

    update logo

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import Logo from '../../asset/image/logo.svg';
+import Logo from '../../asset/image/logo.png';
 
 class AppHeader extends Component {
   render() {
     return (
       <nav class="navbar navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           <img src={Logo} width="50" height="50" alt="" />
         </a>
       </nav>
