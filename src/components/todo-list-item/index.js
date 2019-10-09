efa49a19fa3f658918da9c6e96e393a0650d150f
kdev <kdev.software1110@gmail.com>
2019-10-09 04:59:53 -0400

    todo-list-item logic

--- src/components/todo-list-item/index.js
+++ src/components/todo-list-item/index.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { completeToDo } from '../../store/action';
 
 class TodoListItem extends Component {
   render() {
+    const { todoId, todo } = this.props;
+
     return (
-      <div class="card text-white bg-dark mb-3">
+      <div class="card text-white bg-dark mb-3" key="toDoName">
         <div class="card-body">
-          <p class="card-text">To do content</p>
+          <p class="card-text">{todo.title}</p>
         </div>
 
         <div class="card-footer text-center">
-          <button type="button" class="btn btn-primary">
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={() => this.completeClick(todoId)}
+          >
             Done
           </button>
         </div>
       </div>
     );
   }
+
+  completeClick = completeTodoId => {
+    const { completeToDo, place } = this.props;
+    completeToDo(place, completeTodoId);
+  };
 }
 
-export default TodoListItem;
+export default connect(
+  null,
+  { completeToDo }
+)(TodoListItem);
--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import TodoList from '../../components/todo-list';
+import { HOME, OFFICE } from '../../store/constant';
 
 class AppMain extends Component {
   render() {
@@ -8,12 +9,12 @@ class AppMain extends Component {
       <div class="d-flex justify-content-between">
         <div class="d-flex flex-column">
           <h1>Home</h1>
-          <TodoList />
+          <TodoList place={HOME} />
         </div>
 
         <div class="d-flex flex-column">
           <h1>Office</h1>
-          <TodoList />
+          <TodoList place={OFFICE} />
         </div>
       </div>
     );
