8e81b3e4567d588ea3927a826ad8ffdbaeb46ff8
kdev <kdev.software1110@gmail.com>
2019-10-09 07:49:35 -0400

    Updates

---
+++ precache-manifest.471d54da1c65431dc6964a85892a729e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efce0d13bea16e69e5ed2c6be45ee9ed",
+    "url": "/kdev-todo-list/index.html"
+  },
+  {
+    "revision": "486c5d7e4244f383bbb8",
+    "url": "/kdev-todo-list/static/css/2.20ca5fd7.chunk.css"
+  },
+  {
+    "revision": "ca53a2a4b0c1ccf956bf",
+    "url": "/kdev-todo-list/static/css/main.44ed42bc.chunk.css"
+  },
+  {
+    "revision": "486c5d7e4244f383bbb8",
+    "url": "/kdev-todo-list/static/js/2.e90b2864.chunk.js"
+  },
+  {
+    "revision": "ca53a2a4b0c1ccf956bf",
+    "url": "/kdev-todo-list/static/js/main.fedccdce.chunk.js"
+  },
+  {
+    "revision": "86e805da2cca2d2dbb31",
+    "url": "/kdev-todo-list/static/js/runtime-main.6c6983e0.js"
+  },
+  {
+    "revision": "22e9012a79959598b37c2a0de379ae04",
+    "url": "/kdev-todo-list/static/media/Market.22e9012a.otf"
+  },
+  {
+    "revision": "67c1a1e2487afff18dde4b18d5f22621",
+    "url": "/kdev-todo-list/static/media/logo.67c1a1e2.png"
+  }
+]);
